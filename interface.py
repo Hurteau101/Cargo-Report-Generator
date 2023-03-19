ffb9905a486ba5bb1dc8763a1217d75a82796b13
Devon <Devon@DESKTOP-MCMJABH>
2023-03-19 12:45:20 -0500

    Added login functionality and checks to make sure to handle invalid logins

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from webpage_loader import CargoWebpage
 from dotenv import load_dotenv
 import os
@@ -8,7 +9,7 @@ import tkinter as tk
 
 # Loading the environment variables into constants.
 load_dotenv()
-USERNAME = os.getenv("USERNAME")
+USERNAME = os.getenv("USERNAME_1")
 PASSWORD = os.getenv("PASSWORD")
 URL = os.getenv("CARGO_URL")
 
@@ -16,6 +17,7 @@ URL = os.getenv("CARGO_URL")
 class CargoInterface(ctk.CTk):
     def __init__(self):
         super().__init__()
+        self.webpage = CargoWebpage()
         load_dotenv()
         self.title("Cargo Interface")
         self.geometry("500x350")
@@ -47,7 +49,7 @@ class CargoInterface(ctk.CTk):
 
         # Days Setting Frame
         self.day_frame = ctk.CTkFrame(master=self.setting_frame, height=40, fg_color="transparent")
-        self.day_frame.pack(side="left", padx=(80, 50), pady=(0, 20))
+        self.day_frame.pack(side="left", padx=(120, 50), pady=(0, 20))
 
         # Date Setting Frame
         self.date_frame = ctk.CTkFrame(master=self.setting_frame, height=40, fg_color="transparent")
@@ -57,32 +59,51 @@ class CargoInterface(ctk.CTk):
         self.day_label = ctk.CTkLabel(master=self.day_frame, text="Days")
         self.day_label.pack()
 
-        self.day_box = ctk.CTkEntry(master=self.day_frame)
+        self.day_box = ctk.CTkEntry(master=self.day_frame, width=100, justify="center", placeholder_text="8")
         self.day_box.pack()
+        self.default_day_value()
 
         # Date Setting
         self.date_label = ctk.CTkLabel(master=self.date_frame, text="Date")
         self.date_label.pack()
 
-        self.date_box = ctk.CTkEntry(master=self.date_frame)
+        self.date_box = ctk.CTkEntry(master=self.date_frame, width=100, justify="center",
+                                     placeholder_text="19-Mar-2023")
         self.date_box.pack(side="left")
+        self.default_date_value()
 
         # SLA/Bot Report - Main Layout
-        self.button = ctk.CTkButton(master=self.main_frame, text="Webpage Load")
-        self.button.grid(row=0, column=0)
-        self.spin = tk.Spinbox(master=self.main_frame, from_=0, to=10, bd=3, justify="center", wrap=False)
-        self.spin.grid(row=1, column=0, pady=10)
+        self.button = ctk.CTkButton(master=self.main_frame, text="Webpage Load", command=self.sla_bot_report_click)
+        self.button.pack()
+
 
     def default_day_value(self):
         self.day_box.insert(ctk.END, "8")
 
     def default_date_value(self):
-        today_date = datetime.today().date()
-        self.date_box.insert(ctk.END, today_date.strftime("%d-%b-%Y"))
+        """Set default date. The default date is set 2 months from current date"""
+        default_date = datetime.today().date() - relativedelta(months=2)
+        self.date_box.insert(ctk.END, default_date.strftime("%d-%b-%Y"))
+
+    def _set_credentials(self):
+        self.webpage.username = USERNAME
+        self.webpage.password = PASSWORD
+
+    def _start_selenium(self):
+        self.webpage.start_selenium()
+        self.webpage.load_url(URL)
+
+    def check_login(self):
+        if not self.webpage.login():
+            pass
+            # TODO: Add popup to let user know unsucessful login
 
     def sla_bot_report_click(self):
-        load_dotenv()
-        self.cargo.load_url(os.getenv("CARGO_URL"))
+        self._start_selenium()
+        self._set_credentials()
+        self.check_login()
+
+
 
 cargo = CargoInterface()
 cargo.mainloop()
--- webpage_loader.py
+++ webpage_loader.py
@@ -1,26 +1,31 @@
 from webpage_settings import WebpageSettings
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
 
 
 class CargoWebpage:
-    def __init__(self, options=None):
+    def __init__(self):
         self._username = None
         self._password = None
-        service = Service("chromedriver.exe")
-        self.driver = webdriver.Chrome(service=service, options=options)
-
+        self.driver = None
 
     @property
     def username(self):
-        return self._password
+        return self._username
+
     @username.setter
     def username(self, username):
         self._username = username
 
     @property
     def password(self):
-        return self._username
+        return self._password
 
     @password.setter
     def password(self, password):
@@ -28,4 +33,55 @@ class CargoWebpage:
 
     def load_url(self, url):
         self.driver.get(url)
+
+    def start_selenium(self, options=None):
+        """Use method to start Selenium Webdriver. Use WebpageSettings.headless_chrome as argument to set Selenium to
+        headless mode"""
+        service = Service("chromedriver.exe")
+        self.driver = webdriver.Chrome(service=service, options=options)
+
+    def quit_selenium(self):
+        self.driver.quit()
+
+    def _validate_credentials(self):
+        if self._password is None or self._username is None:
+            self.quit_selenium()
+            raise ValueError("Username and Password must be set before attempting to login.")
+
+    def check_webpage_loaded(self, element: str, wait_time: int, error_message: str = None):
+        """Check if a webpage is loaded or not.
+        Pass in the XPATH string, the wait time length and an optional error message"""
+        try:
+            WebDriverWait(self.driver, wait_time).until(EC.visibility_of_element_located((By.XPATH, element)))
+        except TimeoutException:
+            # TODO: Add popup to let user know page didn't load correctly
+            if error_message is None:
+                print("Webpage Timeout")
+            else:
+                print(error_message)
+            return False
+
+        return True
+
+    def check_login(self):
+        if not self.check_webpage_loaded("//div[@class='DlinkLoggedIn']//a[normalize-space()='Logout']",
+                                         wait_time=5, error_message="Login Failed"):
+            return False
+        return True
+
+    def login(self):
+        self._validate_credentials()
+        if self.check_webpage_loaded("//input[@id='UserName']", 5):
+            username_field = self.driver.find_element(By.XPATH, "//input[@id='UserName']")
+            username_field.send_keys(self.username)
+
+            password_field = self.driver.find_element(By.XPATH, "//input[@id='pwd']")
+            password_field.send_keys(self.password)
+
+            login_button = self.driver.find_element(By.XPATH, "//button[@id='load2']")
+            login_button.click()
+
+            return self.check_login()
+
+        return False
 
