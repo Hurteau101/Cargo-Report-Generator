e32130427012a2ebd8e9139ef96d0217d3c6c5d7
Devon <Devon@DESKTOP-MCMJABH>
2023-03-21 14:32:40 -0500

    Removed duplicate setting_window. Also added script_running switch to allow user to stop the web scrapper script at anytime.

--- test_interface.py
+++ test_interface.py
@@ -7,12 +7,12 @@ import tkinter.messagebox as messagebox
 import threading
 from setting_window import SettingWindow
 
+
 # Loading the environment variables into constants.
 load_dotenv()
 USERNAME = os.getenv("USERNAME_1")
 PASSWORD = os.getenv("PASSWORD")
 URL = os.getenv("CARGO_URL")
-
 
 class CargoInterface(ctk.CTk):
     def __init__(self):
@@ -20,7 +20,7 @@ class CargoInterface(ctk.CTk):
         self.webpage = CargoWebpage()
         load_dotenv()
         self.title("Cargo Interface")
-        self.geometry("500x500")
+        self.geometry("300x300")
         ctk.set_appearance_mode("System")
         ctk.set_default_color_theme("blue")
         self.resizable(False, False)
@@ -43,76 +43,30 @@ class CargoInterface(ctk.CTk):
         self.tabview.add("SLA/Bot Report")
         self.tabview.add("Home Delivery")
 
-        # Main Frame
+        # SLA/Bot Report - Main Frame
         self.main_frame = ctk.CTkFrame(self.tabview.tab("SLA/Bot Report"), fg_color="transparent")
-        self.main_frame.pack()
-        # self.main_frame.pack(fill="x", pady=(0, 10))
+        self.main_frame.pack(fill="x", pady=(0, 10))
 
-        # SLA/Bot Report Frame
-        self.bot_sla_frame = ctk.CTkFrame(master=self.main_frame, height=380)
-        self.bot_sla_frame.pack_propagate(False)
-        self.bot_sla_frame.pack(side="left", pady=(20, 0), padx=(0, 30))
+        # SLA/Bot Report - Main Layout
+        self.main_frame.grid_rowconfigure(0, weight=1)
+        self.main_frame.grid_columnconfigure(0, weight=1)
 
-        # Option Frame
-        self.option_frame = ctk.CTkFrame(master=self.main_frame, height=380)
-        self.option_frame.pack_propagate(False)
-        self.option_frame.pack(side="left", pady=(20, 0))
+        self.text_box = ctk.CTkTextbox(master=self.main_frame)
+        self.text_box.grid(row=0, column=0, pady=(10, 15))
 
-        # SLA/BOT Report Layout
-        self.textbox = ctk.CTkTextbox(master=self.bot_sla_frame)
-        self.textbox.pack()
+        self.button = ctk.CTkButton(master=self.main_frame, text="Webpage Load", width=200,
+                                    command=self.sla_bot_report_command)
+        self.button.grid(row=1, column=0, pady=(0, 10))
 
-        self.webpage_btn = ctk.CTkButton(master=self.bot_sla_frame, text="Webpage Loader",
-                                         command=self.sla_bot_report_command)
-        self.webpage_btn.pack(fill="x", pady="15")
-
-        # Option Layout
-        self.option_label = ctk.CTkLabel(master=self.option_frame, text="Options", font=("Helvetica", 12,
-                                                                                         "bold", "underline"))
-        self.option_label.pack()
-
-        self.hide_text_cb = ctk.CTkCheckBox(master=self.option_frame, text="Hide Text", command=self.hide_textbox)
-        self.hide_text_cb.pack(pady="5")
-
-        self.script_running = ctk.StringVar(value="off")
-
-        self.script_switch = ctk.CTkSwitch(master=self.option_frame, text="Script Running",
-                                           command=self.stop_script_command, variable=self.script_running, onvalue="on",
-                                           offvalue="off", state=ctk.DISABLED)
-        self.script_switch.pack()
-
-    def stop_script(self):
-        """Stop selenium script, set swtich to off and disable switch"""
-        self.set_script_switch("false")
-        self.webpage.quit_selenium()
-
-    def stop_script_command(self):
-        """Create new thread for stopping selenium"""
-        thread = threading.Thread(target=self.stop_script)
-        thread.start()
-
-    def set_script_switch(self, script_on: bool):
-        """Set Script Switch Widget to on or off. True = on | False = off"""
-        if not isinstance(script_on, bool):
-            self.webpage.quit_selenium()
-            raise TypeError(f"Expected 'script_on' to be of type 'bool' but got '{type(script_on)}' instead")
-
-        if script_on:
-            self.script_running = ctk.StringVar(value="on")
-            self.script_switch.configure(state=ctk.NORMAL, variable=self.script_running)
-        else:
-            self.script_running = ctk.StringVar(value="off")
-            self.script_switch.configure(state=ctk.DISABLED, variable=self.script_running)
+        self.checkbox = ctk.CTkCheckBox(master=self.main_frame, text="Hide Text", checkbox_height=20,
+                                        checkbox_width=20, command=self.hide_textbox)
+        self.checkbox.grid(row=2, column=0, pady=(0, 10))
 
     def sla_bot_report_command(self):
-        """Create new thread for starting webscraper"""
         thread = threading.Thread(target=self.sla_bot_report_click)
         thread.start()
 
     def sla_bot_report_click(self):
-        # Set switch value to on and enable switch.
-        self.set_script_switch(True)
-
         self.start_selenium()
         self._set_credentials()
 
@@ -133,13 +87,13 @@ class CargoInterface(ctk.CTk):
             self.setting_window.focus()
 
     def hide_textbox(self):
-        checkbox_state = self.hide_text_cb.get()
+        checkbox_state = self.checkbox.get()
         if checkbox_state == 1:
-            self.textbox.pack_forget()
+            self.text_box.grid_remove()
+            self.geometry("250x130")
         else:
-            self.webpage_btn.pack_forget()
-            self.textbox.pack()
-            self.webpage_btn.pack(pady="15")
+            self.text_box.grid(row=0, column=0)
+            self.geometry("300x300")
 
     def _set_credentials(self):
         self.webpage.username = USERNAME
@@ -173,3 +127,6 @@ class CargoInterface(ctk.CTk):
 
 cargo = CargoInterface()
 cargo.mainloop()
+
+
+
