b35a38b6350c21d495d640cf54ca5e3e757e7c88
Devon <Devon@DESKTOP-MCMJABH>
2023-03-23 21:47:30 -0500

    Added the ability to update the settings to the database

--- Settings_Data.py
+++ Settings_Data.py
@@ -3,14 +3,17 @@ from utils import type_check
 
 
 class SettingsData:
+    BOT_SLA_REPORT_TABLE_NAME = "BotReportSettings"
+    HOME_REPORT_TABLE_NAME = "HomeReportSettings"
+
     def __init__(self):
         self.connector = DatabaseConnector()
 
     def get_bot_sla_data(self):
-        return self.get_setting_data("BotReportSettings")
+        return self.get_setting_data(self.BOT_SLA_REPORT_TABLE_NAME)
 
     def get_home_delivery_data(self):
-        return self.get_setting_data("HomeReportSettings")
+        return self.get_setting_data(self.HOME_REPORT_TABLE_NAME)
 
     def get_setting_data(self, table_name: str):
         """
@@ -21,7 +24,7 @@ class SettingsData:
         """
         type_check(arg=table_name, arg_name="table_name", expected_type=str)
 
-        valid_tables = ["BotReportSettings", "HomeReportSettings"]
+        valid_tables = [self.HOME_REPORT_TABLE_NAME, self.BOT_SLA_REPORT_TABLE_NAME]
 
         if table_name not in valid_tables:
             raise ValueError(f"{table_name} is not a valid table in the database")
@@ -36,3 +39,35 @@ class SettingsData:
         settings_data = {column_names[i]: row[i] for i in range(len(column_names))}
 
         return settings_data
+
+    # TODO: Add doctsring
+    def update_sla_bot_settings(self, table_name: str, **kwargs):
+        day_amount = kwargs.get("DayAmount")
+        num_of_months = kwargs.get("NumOfMonths")
+        num_of_days = kwargs.get("NumOfDays")
+        from_airport = kwargs.get("FromAirport")
+        to_airport = kwargs.get("ToAirport")
+
+        self.connector.connect()
+        cursor = self.connector.connection.cursor()
+        cursor.execute(f"UPDATE {table_name} SET DayAmount = ?, NumOfMonths = ?, "
+                       f"NumOfDays = ?, FromAirport = ?, ToAirport = ?;",
+                       (day_amount, num_of_months, num_of_days, from_airport, to_airport))
+        self.connector.connection.commit()
+
+    # TODO: Add doctsring
+    def update_home_settings(self, table_name: str, **kwargs):
+        keywords = kwargs.get("Keyword")
+        num_of_months = kwargs.get("NumOfMonths")
+        num_of_days = kwargs.get("NumOfDays")
+        from_airport = kwargs.get("FromAirport")
+        to_airport = kwargs.get("ToAirport")
+
+        self.connector.connect()
+        cursor = self.connector.connection.cursor()
+        cursor.execute(f"UPDATE {table_name} SET Keyword = ?, NumOfMonths = ?, "
+                       f"NumOfDays = ?, FromAirport = ?, ToAirport = ?;",
+                       (keywords, num_of_months, num_of_days, from_airport, to_airport))
+        self.connector.connection.commit()
+
+
--- setting_window.py
+++ setting_window.py
@@ -22,9 +22,18 @@ class SettingWindow(ctk.CTkToplevel):
         self.create_bot_sla_frame()
         self.create_home_delivery_frame()
 
-        # # Insert Widgets on Setting Window
-        self.create_widgets(self.bot_sla_frame, self.sla_bot_widgets_data())
-        self.create_widgets(self.home_frame, self.home_widgets_data())
+        # Insert the widgets on Setting Window.
+        # Widgets are being stored in a dictionary, so you can access the entry fields later on.
+        self.widget_list = {
+            "SLA/Bot Widgets": self.create_widgets(self.bot_sla_frame, self.sla_bot_widgets_data()),
+            "Home Delivery Widgets": self.create_widgets(self.home_frame, self.home_widgets_data())
+        }
+
+        # Store the previous values
+        self.previous_values = {
+            "SLA/Bot Values": self.get_entry_box_values("SLA/Bot Widgets"),
+            "Home Delivery Values": self.get_entry_box_values("Home Delivery Widgets")
+        }
 
     def create_main_frame(self):
         """Main Setting Frame"""
@@ -48,7 +57,8 @@ class SettingWindow(ctk.CTkToplevel):
         save_button_frame = ctk.CTkFrame(master=self.main_frame, fg_color="transparent")
         save_button_frame.pack(side="bottom")
 
-        self.save_button = ctk.CTkButton(master=save_button_frame, text="Save Settings", width=430)
+        self.save_button = ctk.CTkButton(master=save_button_frame, text="Save Settings", width=430,
+                                         command=self.save_setting)
         self.save_button.pack(pady="20")
 
     @classmethod
@@ -90,8 +100,8 @@ class SettingWindow(ctk.CTkToplevel):
         """Insert the settings that were retrieved from the database.
             :param widget_data: Dictionary list of widgets
             :param setting_group: Select which setting dictionary to load. Valid values are:
-                              bot_report_settings and
-                              home_delivery_settings
+                              bot_sla_data and
+                              home_data
         """
         # Enumerate to avoid access the first element in list, as that is header text.
         for index, data in enumerate(widget_data):
@@ -102,6 +112,11 @@ class SettingWindow(ctk.CTkToplevel):
     @classmethod
     def create_widgets(cls, frame, widget_data):
         """Insert all the widgets on setting screen"""
+
+        # Create a dictionary to store the entry widgets
+        # The keys of the dictionary are the labels for that entry field in the widget_data list
+        widget_list = {}
+
         # Using enumerate to access the first item in the list, as that will be the header text.
         for index, field in enumerate(widget_data):
             if index == 0:
@@ -117,16 +132,21 @@ class SettingWindow(ctk.CTkToplevel):
                 entry.insert(ctk.END, field["setting_key"])
                 entry.pack()
 
+                key = f"{field['label_text']}"
+                widget_list[key] = entry
+
+        return widget_list
+
     @classmethod
     def load_setting(cls, setting_group: dict, setting_name: str):
         """
         Load the specified setting.
 
         :param setting_group: Select which setting dictionary to load. Valid values are:
-                              bot_report_settings and
-                              home_delivery_settings
+                              bot_sla_data and
+                              home_data
         :param setting_name: Load the values of the specified settings.
-                             If setting_group is bot_report_settings, valid values are:
+                              Valid values are:
                               DayAmount,
                               NumOfMonths,
                               NumOfDays,
@@ -141,6 +161,62 @@ class SettingWindow(ctk.CTkToplevel):
         :return: Returns the value for the specified setting name.
         """
         return setting_group[setting_name]
+
+    def save_sla_bot_settings(self):
+        """Update SLA/Bot Setting Database values."""
+        day_amount = self.widget_list["SLA/Bot Widgets"]["Days"].get()
+        num_of_months = self.widget_list["SLA/Bot Widgets"]["Number of Months Back"].get()
+        num_of_days = self.widget_list["SLA/Bot Widgets"]["Number of Days Back"].get()
+        from_airport = self.widget_list["SLA/Bot Widgets"]["From Airport"].get()
+        to_airport = self.widget_list["SLA/Bot Widgets"]["To Airport"].get()
+
+        self.setting_data.update_sla_bot_settings(table_name=self.setting_data.BOT_SLA_REPORT_TABLE_NAME,
+                                                  DayAmount=int(day_amount), NumOfMonths=int(num_of_months),
+                                                  NumOfDays=int(num_of_days), FromAirport=from_airport,
+                                                  ToAirport=to_airport)
+
+    def save_home_settings(self):
+        """Update Home Setting Database values."""
+        keyword = self.widget_list["Home Delivery Widgets"]["Keyword"].get()
+        num_of_months = self.widget_list["Home Delivery Widgets"]["Number of Months Back"].get()
+        num_of_days = self.widget_list["Home Delivery Widgets"]["Number of Days Back"].get()
+        from_airport = self.widget_list["Home Delivery Widgets"]["From Airport"].get()
+        to_airport = self.widget_list["Home Delivery Widgets"]["To Airport"].get()
+
+        self.setting_data.update_home_settings(table_name=self.setting_data.HOME_REPORT_TABLE_NAME, Keyword=keyword,
+                                               NumOfMonths=int(num_of_months), NumOfDays=int(num_of_days),
+                                               FromAirport=from_airport, ToAirport=to_airport)
+
+    def get_entry_box_values(self, widget_group):
+        """
+        Get the current entry box values.
+        :param widget_group: Retrieve the specified setting's widget group's values.
+                            valid values are:
+                            SLA/Bot Widgets and
+                            Home Delivery Widgets
+        :return: return all current entry box values.
+        """
+        widget_values = {key: value.get() for (key, value) in self.widget_list[widget_group].items()}
+        return widget_values
+
+    def save_setting(self):
+        """
+        Check which setting group was changed. If setting group was changed, then update that setting group to database
+        """
+        current_values = {
+            "SLA/Bot Values": self.get_entry_box_values("SLA/Bot Widgets"),
+            "Home Delivery Values": self.get_entry_box_values("Home Delivery Widgets")
+        }
+
+        if current_values["SLA/Bot Values"] != self.previous_values["SLA/Bot Values"]:
+            self.previous_values = current_values
+            self.save_sla_bot_settings()
+
+        if current_values["Home Delivery Values"] != self.previous_values["Home Delivery Values"]:
+            self.previous_values = current_values
+            self.save_home_settings()
+
+    #TODO: Update entry box values once settings are saved.
 
     def on_focus_in(self, event):
         """Bring setting window to the front when opened"""
