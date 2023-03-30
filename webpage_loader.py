d58f5fa0eddd14bf5427cd8ae5f59ef84ad9a018
Devon <Devon@DESKTOP-MCMJABH>
2023-03-30 18:26:28 -0500

    Added search_awb, check_home_delivery_text, get_status_of_awb, awb_status_not_found and results_found methods. All methods added will interact with each other to obtain a list of Home Delivery AWB's and extract information about the Home Delivery AWB's

--- webpage_loader.py
+++ webpage_loader.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+import requests
 from datetime import date
 import time
 from requests import Request, Session
@@ -44,6 +45,7 @@ class CargoWebpage:
         :return: None
         """
         self.driver.get(url)
+        return self.driver.get_cookies()
 
     def start_selenium(self, options: webdriver.ChromeOptions = None):
         """
@@ -164,7 +166,7 @@ class CargoWebpage:
 
         return waybill_html, sla_bot_data["DayAmount"]
 
-    def search_awbs(self, url: str) -> bool:
+    def check_search_awbs_page(self, url: str) -> bool:
         self.load_url(url)
         if self.check_element_loaded("//input[@id='txt_key']", wait_time=5):
             return True
@@ -190,7 +192,7 @@ class CargoWebpage:
         search_button.click()
 
         if self.results_found():
-            # Allows the hidden dropdown box to appear.
+            # Allows the hidden dropdown box to appear by waiting 1 second.
             time.sleep(1)
             items_per_page_drop_down = self.driver.find_element(By.XPATH,
                                                                 "/html/body/div[7]/div[5]/div[2]/div/div[2]/span["
@@ -212,5 +214,100 @@ class CargoWebpage:
         else:
             print("Failed")
 
+    def search_awb(self, awb_list: list):
+        """
+        Method will load the search AWB page. It will loop through the list of AWB's passed in as an argument (List of
+        dictionarys of AWB's). It will then check if it finds certain text on the AWB Pop Up Modal Page.
+        If it does it will add the Flight information to that specific dictionary in the awb_info list. If it doesn't
+        it will remove that dictionary of that awb from awb_info list. Returns the list of home delivery AWB's.
+        :param awb_list: List of dictionarys that contain AWB's.
+        :return: List of dictionarys that are the Home Delivery AWB's.
+        """
+        self.load_url(self.webpage_data.get_search_awb_url())
+        awb_field = self.driver.find_element(By.XPATH, "/html/body/div[7]/form/div/div[1]"
+                                                       "/div/div/div[1]/div[1]/div/div[2]/input")
+        close_awb_modal = self.driver.find_element(By.XPATH, "/html/body/div[7]/div[6]/div/div/div[1]/button")
+        search_button = self.driver.find_element(By.XPATH, "//button[@id='btn_search']")
+
+        awb_info = awb_list.copy()
+
+        for i in range(len(awb_list)):
+            awb = awb_list[i].get("AWB No.")
+
+            awb_field.send_keys(awb)
+            search_button.click()
+            if self.check_home_delivery_text():
+                awb_info[i].update(self.get_status_of_awb())
+            else:
+                awb_info.pop(i)
+            close_awb_modal.click()
+            awb_field.clear()
+
+        self.quit_selenium()
+
+        return awb_info
+
+    def check_home_delivery_text(self):
+        """
+        Check if "Home Delivery" text is located on the AWB Page. If it is return true, otherwise return False
+        :return: True if "Home Delivery" text found | False if "Home Delivery" NOT found.
+        """
+        if self.check_element_loaded("/html/body/div[7]/div[6]/div/div/div[2]/div/div/div[2]/div/div/div[2]/div[3]",
+                                     wait_time=5):
+            awb_page = self.driver.find_element(By.XPATH, "/html/body/div[7]/div[6]/div/div/div[2]/div/div/div[2]")
+
+            if "HOME DELIVERY" in awb_page.text:
+                return True
+            return False
+
+    def get_status_of_awb(self):
+        """
+        Check if the AWB has been shipped or if it's waiting to be shipped. There is a Try/Except block, in the off
+        chance an AWB was created inproperly. If an AWB is created inproperly, it will get the status message of that
+        AWB and return it back to specific awb dictionary. If there is no error, it will obtain the shipped flight
+        information about that AWB and return it back to that AWB.
+        :return: Returns a dictionary of flight information or status information (if AWB isn't created properly).
+        """
+        try:
+            table = self.driver.find_element(By.XPATH, "/html/body/div[7]/div[6]/div/div/div[2]"
+                                                   "/div/div/div[2]/div/div/div[2]/div[10]/div/div[2]/div/table")
+        except NoSuchElementException:
+            return self.awb_status_not_found()
+        else:
+            rows = table.find_elements(By.TAG_NAME, "tr")
+            row = rows[-1]
+
+            cells = row.find_elements(By.TAG_NAME, "td")
+            cell_list = [cell.text for cell in cells]
+
+            row_header = table.find_elements(By.TAG_NAME, "th")
+            row_header_list = [row.text for row in row_header]
+
+            status_dict = {row_header_list[i]: cell_list[i] for i in range(len(row_header_list))
+                           if row_header_list[i] in ['Flight Status', 'Flight Number', 'Flight Date', "Weight"]}
+
+            return status_dict
+
+    def awb_status_not_found(self):
+        """
+        If AWB is created incorrectly, this method is called and will find the status information about the AWB and
+        return it as a dictionary.
+        :return: Returns a dictionary about the status information of the AWB.
+        """
+        table = self.driver.find_element(By.XPATH, "/html/body/div[7]/div[6]/div/div/div[2]"
+                                              "/div/div/div[2]/div/div/div[2]/div[8]/div/div[2]/div/table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        last_row = rows[-1]
+        last_cell = last_row.find_elements(By.TAG_NAME, "td")
+
+        status_dict = {"Flight Status": last_cell[-1].text}
+
+        return status_dict
+
     def results_found(self):
+        """
+        Calls the check_element_loaded to see if the AWB Pop Up Modal is properly loaded before trying to locate text.
+        It will wait 5 seconds before returning False.
+        :return: Returns True if it can find the table in the AWB Pop Up Modal otherwise False if it can't.
+        """
         return self.check_element_loaded(element="/html/body/div[7]/div[5]/div[2]/div/table", wait_time=3)
