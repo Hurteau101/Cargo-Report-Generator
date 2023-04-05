8db0583e27876a7408762b4a633cee65033ed74b
Devon <Devon@DESKTOP-MCMJABH>
2023-04-05 11:10:30 -0500

    Changed docstring

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 def type_check(arg, arg_name: str, expected_type: type):
+    """
+    Check the type of argument
+    :param arg: The argument.
+    :param arg_name: The argument name.
+    :param expected_type: The type expected.
+    :raise KeyError: Raises an error, if the argument is not what is expected
+    """
     """Check the type of argument. Raise an error, if the argument is not what is expected"""
     if type(arg) != expected_type:
         raise TypeError(f"Expected '{arg_name}' to be of type '{expected_type.__name__}' "
